6fc91421e7b3f5d874e94133605d3ca52bf3a1ba
Vanessa Montoya <Vanessa@C4Qs-MacBook-Pro.local>
2016-09-08 16:54:44 -0400

    test

--- part-1-ajax/test.js
+++ part-1-ajax/test.js
@@ -1,76 +1,87 @@
-function getCountryName(code) {
-    $.ajax({
-      //uses the three letter code to get the country's full name
-      }
-    })
-  }
+// $('button').on('click', function(){
 
-  function parseData(country) {
+// })
+
+//https://restcountries.eu/rest/v1/name/united%20states?fullText=true
+// function getCountryName(code) {
+
+
+//     $.ajax({
+//         //uses the three letter code to get the country's full name
+
+//         sucess: function(data) {
+//             //console.log(data)
+//             var name = data[0].name
+//             console.log(name)
+//         },
+//         error: function() {
+//             $('body').append('<h1>Not Found</h1>')
+//         },
+
+//     })
+// }
+
+function parseData(country) {
     //takes the country data
-    //extracts the name, capital, population, and neighboring countries
-    //iterates through the neighbors array to send off second AJAX request
-  }
+    //var neighbors =
+        // for (var i = 0; i < neighbors.length; i++) {
 
-  var button = document.getElementsByTagName('button')[0]
-  button.addEventListener('click', function(){
-
+        // }
+    var rooturl = 'https://restcountries.eu/rest/v1/name/';
+    var country = $('input').val().toLowerCase().split(' ').join('%20');
+    var urlend = '?fullText=true';
     $.ajax({
+        url: rooturl + country + urlend,
+        success: function(data) {
+          // console.log(data)
+          //var name = data[0].name
+          //$('.name').append(name))
+        },
+        error: function() {
+          $('body').append('<h1>Not Found</h1>')
+        },
 
     })
-  })
+        //extracts the name, capital, population, and neighboring countries
+        //iterates through the neighbors array to send off second AJAX request
+}
 
-//When you search for a country you should receive a response that looks like:
-[ {
-      "name": "Argentina",
-      "topLevelDomain": [
-          ".ar"
-      ],
-      "alpha2Code": "AR",
-      "alpha3Code": "ARG",
-      "currencies": [
-          "ARS"
-      ],
-      "callingCodes": [
-          "54"
-      ],
-      "capital": "Buenos Aires",
-      "altSpellings": [
-          "AR",
-          "Argentine Republic",
-          "República Argentina"
-      ],
-      "relevance": "0",
-      "region": "Americas",
-      "subregion": "South America",
-      "languages": [
-          "es",
-          "gn"
-      ],
-      "translations": {
-          "de": "Argentinien",
-          "es": "Argentina",
-          "fr": "Argentine",
-          "ja": "アルゼンチン",
-          "it": "Argentina"
-      },
-      "population": 43131966,
-      "latlng": [
-          -34,
-          -64
-      ],
-      "demonym": "Argentinean",
-      "area": 2780400,
-      "gini": 44.5,
-      "timezones": [
-          "UTC−03:00"
-      ],
-      "borders": [
-          "BOL",
-          "BRA",
-          "CHL",
-          "PRY",
-          "URY"
-      ],
-      "nativeName": "Argentina"
-  }
-]
+var button = document.getElementsByTagName('button')[0]
+button.addEventListener('click', function() {
+    var rooturl = 'https://restcountries.eu/rest/v1/name/';
+    var country = $('input').val().toLowerCase().split(' ').join('%20');
+    var urlend = '?fullText=true';
+    $.ajax({
+        url: rooturl + country + urlend,
+        success: function(data) {
+          console.log(data)
+        },
+        error: function() {
+          $('body').append('<h1>Not Found</h1>')
+        },
+
+    })
+})
+
+//code provided/template
+// function getCountryName(code) {
+//   $.ajax({
+//     //uses the three letter code to get the country's full name
+//     }
+//   })
+// }
+
+// function parseData(country) {
+//   //takes the country data
+//   //extracts the name, capital, population, and neighboring countries
+//   //iterates through the neighbors array to send off second AJAX request
+// }
+
+// var button = document.getElementsByTagName('button')[0]
+// button.addEventListener('click', function(){
+
+//   $.ajax({
+
+//   })
+// })
+
