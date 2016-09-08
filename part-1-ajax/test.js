6592e060488b264fb792944d4f76bac051d8e386
electrone901 <Carbajalluis@rocketmail.com>
2016-09-08 18:56:21 -0400

    test

--- part-1-ajax/test.js
+++ part-1-ajax/test.js
@@ -1,76 +1,58 @@
-function getCountryName(code) {
-    $.ajax({
-      //uses the three letter code to get the country's full name
+
+$('form').submit(function(event) {
+  event.preventDefault()
+  var input = $(this).serializeArray()[0].value
+  input = input.split(" ").join("+")
+  input = input + "?fullText=true"
+  $('.first').val('');
+
+  $.ajax({
+  
+
+    url: "https://restcountries.eu/rest/v1/name/" + input,
+    success: function(data) {
+
+
+
+      console.log('object return',data)
+
+
+
+      $('#borders').append('<li>Name: '+data[0].name+'</li>');
+
+      $('#borders').append('<li>Capital: '+data[0].capital+'</li>');
+
+      $('#borders').append('<li>Population: '+data[0].population+'</li>');
+
+
+
+
+       var  allBorders = data[0].borders;
+       console.log('allBorders: ', allBorders)
+
+
+     for (var i = 0; i < allBorders.length; i++) {
+       
+       $.ajax({
+
+      url: "https://restcountries.eu/rest/v1/alpha/" + allBorders[i],
+      success: function(data) {
+         console.log('getting data back:', data)
+         console.log('luis country:', data.name)
+
+         var countryName = document.createElement('li')
+          countryName.innerHTML = data.name
+          $('#ul').append(countryName) 
+
+        
+
       }
     })
+
+      }
+    
   }
 
-  function parseData(country) {
-    //takes the country data
-    //extracts the name, capital, population, and neighboring countries
-    //iterates through the neighbors array to send off second AJAX request
-  }
-
-  var button = document.getElementsByTagName('button')[0]
-  button.addEventListener('click', function(){
-
-    $.ajax({
-
-    })
   })
+})
 
-//When you search for a country you should receive a response that looks like:
-[ {
-      "name": "Argentina",
-      "topLevelDomain": [
-          ".ar"
-      ],
-      "alpha2Code": "AR",
-      "alpha3Code": "ARG",
-      "currencies": [
-          "ARS"
-      ],
-      "callingCodes": [
-          "54"
-      ],
-      "capital": "Buenos Aires",
-      "altSpellings": [
-          "AR",
-          "Argentine Republic",
-          "República Argentina"
-      ],
-      "relevance": "0",
-      "region": "Americas",
-      "subregion": "South America",
-      "languages": [
-          "es",
-          "gn"
-      ],
-      "translations": {
-          "de": "Argentinien",
-          "es": "Argentina",
-          "fr": "Argentine",
-          "ja": "アルゼンチン",
-          "it": "Argentina"
-      },
-      "population": 43131966,
-      "latlng": [
-          -34,
-          -64
-      ],
-      "demonym": "Argentinean",
-      "area": 2780400,
-      "gini": 44.5,
-      "timezones": [
-          "UTC−03:00"
-      ],
-      "borders": [
-          "BOL",
-          "BRA",
-          "CHL",
-          "PRY",
-          "URY"
-      ],
-      "nativeName": "Argentina"
-  }
-]
---
+++ part-2-server/createPoemFiles.js
@@ -0,0 +1,44 @@
+var poem = [ 'Take', 'this', 'kiss', 'upon', 'the',
+  'brow!','And,','in','parting','from',
+  'you','now,','Thus','much','let','me',
+  'avow','You','are','not','wrong,',
+  'who','deem','That','my','days',
+  'have','been','a','dream;',
+  'Yet','if','hope','has','flown','away',
+  'In','a','night,','or','in','a','day,',
+  'In','a','vision,','or','in','none,',
+  'Is','it','therefore','the','less','gone?',
+  'All','that','we','see','or','seem','Is',
+  'but','a','dream','within','a','dream.',
+  'I','stand','amid','the','roar',
+  'Of','a','surf-tormented','shore,',
+  'And','I','hold','within','my','hand','Grains',
+  'of','the','golden','sand--',
+  'How','few!','yet','how','they','creep',
+  'Through','my','fingers','to','the','deep,',
+  'While','I','weep--while','I','weep!','O',
+  'God!','can','I','not','grasp','Them','with',
+  'a','tighter','clasp?',
+  'O','God!','can','I','not','save',
+  'One','from','the','pitiless','wave?',
+  'Is','all','that','we','see','or','seem',
+  'But','a','dream','within','a','dream?' ]
+
+
+// Within that same file use the fs module to write a function that will create 140 new text (.txt) files, and insert a word from the poem into each file. The files should be called poem-0.txt, poem-1.txt, poem-2.txt, etc., all the way up to poem-139.txt. Make sure the files end in '.txt'. Each file should contain one word from the poem, so for example, poem-0.txt should contain the first word from the poem, poem-9.txt should contain the tenth word, etc. When you are ready, run the createPoemFiles.js script.
+
+var fs = require('fs');
+
+for (var i = 1; i < 140; i++){
+  fs.writeFile('poem-' + i + '.txt',poem[i + 1]);
+  console.log(i)
+ }
+
+
+
+
+ 
+
+
+
+
